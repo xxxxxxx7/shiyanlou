463b2f863687537106d52c63c92016fd9de9720a
914241406 <914241406@qq.com>
2017-11-19 22:06:10 +0800

    change

--- day2/calculator.py
+++ day2/calculator.py
@@ -2,32 +2,46 @@
 
 import sys
 
-arg = sys.argv[1]
+#arg = sys.argv[1]
 
 try:
-	if int(arg) < 3500:
-		print("0")
-	elif int(arg) - 3500 <= 1500:
-		a = (int(arg) - 3500) * 0.03
-		print(format(a,".2f"))
-	elif int(arg) - 3500 >= 1500 and int(arg) - 3500 <= 4500:
-		a = (int(arg) - 3500) * 0.1 - 105
-		print(format(a,".2f"))
-	elif (int(arg) - 3500) >= 4500 and (int(arg) - 3500) <= 9000:
-		a = (int(arg) - 3500) * 0.2 - 555
-		print(format(a,".2f"))
-	elif int(arg) - 3500 >= 9000 and int(arg) - 3500 <= 35000:
-		a = (int(arg) - 3500) * 0.25 - 1005
-		print(format(a,".2f"))
-	elif int(arg) - 3500 >= 35000 and int(arg) - 3500 <= 55000:
-		a = (int(arg) - 3500) * 0.3 - 2755
-		print(format(a,".2f"))
-	elif int(arg) - 3500 >= 55000 and int(arg) - 3500 <= 80000:
-		a = (int(arg) - 3500) * 0.35 - 5505
-		print(format(a,".2f"))
-	elif int(arg) - 3500 >= 80000:
-		a = (int(arg) - 3500) * 0.45 - 13505
-		print(format(a,".2f"))
+	if (len(sys.argv)) < 1:
+            raise ValueError
+
+	for arg in sys.argv[1:]:
+		a = arg.split(':')
+		b = float(a[1]) * 0.165
+
+		if int(a[1]) - b < 3500:
+			d = int(a[1]) - b
+		elif int(a[1]) - 3500  <= 1500:
+			c = (int(a[1]) - 3500 - b) * 0.03
+			d = int(a[1]) - b - c
+		elif int(a[1]) - 3500 >= 1500 and int(a[1]) - 3500 <= 4500:
+			c = (int(a[1]) - 3500 -b) * 0.1 - 105
+			d = int(a[1]) - b - c
+#			print(format(a,".2f"))
+		elif (int(a[1]) - 3500) >= 4500 and (int(a[1]) - 3500) <= 9000:
+			c = (int(a[1]) - 3500 -b) * 0.2 - 555
+			d = int(a[1]) - b - c
+#			print(format(a,".2f"))
+		elif int(a[1]) - 3500 >= 9000 and int(a[1]) - 3500 <= 35000:
+			c = (int(a[1]) - 3500 -b) * 0.25 - 1005
+			d = int(a[1]) - b - c
+#			print(format(a,".2f"))
+		elif int(a[1]) - 3500 >= 35000 and int(a[1]) - 3500 <= 55000:
+			c = (int(a[1]) - 3500 -b) * 0.3 - 2755
+			d = int(a[1]) - b - c
+#			print(format(a,".2f"))
+		elif int(a[1]) - 3500 >= 55000 and int(a[1]) - 3500 <= 80000:
+			c = (int(a[1]) - 3500 -b) * 0.35 - 5505
+			d = int(a[1]) - b - c
+#			print(format(a,".2f"))
+		elif int(a[1]) - 3500 >= 80000:
+			c = (int(a[1]) - 3500 -b) * 0.45 - 13505
+			d = int(a[1]) - b - c
+		
+		print(a[0]+":"+format(d,".2f"))
 
 
 
